bd63a8f5e7693bc69a82a84edcba62427c0e80fc
Syed <82619273+Syedm-hub@users.noreply.github.com>
2021-08-20 22:09:29 -0700

    updating with db.json

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static('public'));
 
 
 app.get('/api/notes', (req,res) => {
-    res.json(a)
+    res.json(allNotes.slice);
 });
 
 app.get('/', (req,res) => {
@@ -32,4 +32,58 @@ app.get('/notes', (req, res) => {
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+//creating new notes 
+function createNewNote(body, notesArray) {
+    const newnote = body;
+    if (notesArray.length === 0)
+    notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray)
+    );
+
+    return newNote;
+}
+
+
+//post new notes 
+app.post('/api/notes', (req,res) => {
+    const newNote = createNewNote(req.body, allNotes);
+    res.json(newNote)
+});
+
+
+//delete notes 
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+
+        if (note.id == id) {
+            notesArray.splice();
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(notesArray)
+            );
+
+            break;
+
+        }
+    }
+}
+
+app.delete('/', (req,res) => {
+    deleteNote(req.params.id, allNotes);
+    res.json(true);
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}!`);
 });
