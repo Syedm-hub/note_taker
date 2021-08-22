6179746cac34f5082ba46b95ff1201a4731540a3
Syed <82619273+Syedm-hub@users.noreply.github.com>
2021-08-22 13:08:03 -0700

    updated

--- public/assets/index.js
+++ public/assets/index.js
@@ -151,10 +151,7 @@ const renderNoteList = async (notes) => {
     return liEl;
   };
 
-  if (jsonNotes.length === 0) {
-    noteListItems.push(createLi('No saved Notes', false));
-  }
-
+  
   jsonNotes.forEach((note) => {
     const li = createLi(note.title);
     li.dataset.note = JSON.stringify(note);
