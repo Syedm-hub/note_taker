884ecc64bd80efe3f078c4db342aa7e1e921a301
Syed <82619273+Syedm-hub@users.noreply.github.com>
2021-08-22 15:32:54 -0700

    ready to run

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
+//requiring ports
 const PORT = process.env.PORT || 3001;
-const fs = require('fs');
-const path = require('path');
 
+//requiring modules
+const path = require('path');
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
+
+
+
+
 const allNotes = require('./db/db.json');
 
-app.use(express.urlencoded({ extended: true }));
+//middlewares 
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/api/notes', (req, res) => {
+
+app.get('/api/notes', (req,res) => {
     res.json(allNotes.slice(1));
 });
 
-app.get('/', (req, res) => {
+app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
@@ -27,13 +36,12 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+
+//creating new notes 
 function createNewNote(body, notesArray) {
     const newNote = body;
-    if (!Array.isArray(notesArray))
-        notesArray = [];
-    
     if (notesArray.length === 0)
-        notesArray.push(0);
+    notesArray.push(0);
 
     body.id = notesArray[0];
     notesArray[0]++;
@@ -41,37 +49,43 @@ function createNewNote(body, notesArray) {
     notesArray.push(newNote);
     fs.writeFileSync(
         path.join(__dirname, './db/db.json'),
-        JSON.stringify(notesArray, null, 2)
+        JSON.stringify(notesArray)
     );
+
     return newNote;
 }
 
-app.post('/api/notes', (req, res) => {
+
+//post new notes 
+app.post('/api/notes', (req,res) => {
     const newNote = createNewNote(req.body, allNotes);
-    res.json(newNote);
+    res.json(newNote)
 });
 
+
+//delete notes 
 function deleteNote(id, notesArray) {
     for (let i = 0; i < notesArray.length; i++) {
         let note = notesArray[i];
 
         if (note.id == id) {
-            notesArray.splice(i, 1);
+            notesArray.splice(i,1);
             fs.writeFileSync(
-                path.join(__dirname, './db/db.json'),
+                path.join(__dirname, '/db/db.json'),
                 JSON.stringify(notesArray, null, 2)
             );
 
             break;
+
         }
     }
 }
 
-app.delete('/api/notes/:id', (req, res) => {
+app.delete('/api/notes/:id', (req,res) => {
     deleteNote(req.params.id, allNotes);
     res.json(true);
 });
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+    console.log(`Listening on ${PORT}!`);
 });
